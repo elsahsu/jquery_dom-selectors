f91a85ed8eefc9996d126c3dd857d58d6eccc244
Elsa Hsu <elsanfhsu@gmail.com>
2018-02-06 22:35:53 +0200

    select by tags&classes

---
+++ select_by_classes/select_by_classes.js
@@ -0,0 +1,9 @@
+/*
+Use a jQuery class selector to grab all of the elements of class 'article-item' on the page!
+*/
+
+//start with this variable!
+var articleItems;
+
+articleItems = $('.article-item');
+console.log(articleItems);
---
+++ select_by_tags.js
@@ -0,0 +1,8 @@
+/*
+use a jQuery tag selector to grab all of the <li>s on the page!
+*/
+// Start with this variable!
+var listElements;
+
+listElements = $('li');
+console.log(listElements);
